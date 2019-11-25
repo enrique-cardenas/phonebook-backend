6b40d58ae2d9bf05d88a3c2e64ef498cbb76a55a
Enrique <enriquecardenas96@gmail.com>
2019-11-24 23:20:11 -0800

    uibuild

---
+++ build/precache-manifest.916c871a86973ae1904347e777a594fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534311ef80bf5d815c66994979a336aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "451ed7251bf9041ed64c",
+    "url": "/static/css/main.44d7bb3a.chunk.css"
+  },
+  {
+    "revision": "6bb1e13f260a4a35608e",
+    "url": "/static/js/2.75adb06f.chunk.js"
+  },
+  {
+    "revision": "451ed7251bf9041ed64c",
+    "url": "/static/js/main.e876921e.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
